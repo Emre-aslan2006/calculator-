a3c8eecfb25409f2fe5a0ecb2c66263739f57f41
Emre-aslan2006 <emrealexaslan2006@gmail.com>
2025-04-04 19:44:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const display = document.getElementById('display');
+
+  window.append = function(value) {
+    display.value += value;
+  };
+
+  window.clearDisplay = function() {
+    display.value = '';
+  };
+
+  window.deleteLast = function() {
+    display.value = display.value.slice(0, -1);
+  };
+
+  window.calculate = function() {
+    try {
+      display.value = eval(display.value);
+    } catch {
+      display.value = 'Error';
+    }
+  };
+});
